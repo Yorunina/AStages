3ff1273c19cfc6fad60815831294c04ab33650ac
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2024-11-06 20:36:26 +0100

    Solved different issues

--- src/main/java/com/alessandro/astages/command/argument/AStagesAddArgument.java
+++ src/main/java/com/alessandro/astages/command/argument/AStagesAddArgument.java
@@ -1,6 +1,5 @@
 package com.alessandro.astages.command.argument;
 
-import com.alessandro.astages.capability.PlayerStageProvider;
 import com.alessandro.astages.core.ARestrictionManager;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
@@ -11,19 +10,13 @@ import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.SharedSuggestionProvider;
-import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.EntitySelector;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class AStagesAddArgument implements ArgumentType<String> {
     private static final Collection<String> EXAMPLES = Arrays.asList("test_stage_1", "test_stage_2");
