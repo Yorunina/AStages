040c083ff50108e2980035ba85828cae74eb681d
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-02-08 13:55:17 +0100

    Removed FTB Quests integration

--- src/main/java/com/alessandro/astages/integration/ftbquest/AStageRewardType.java
+++
@@ -1,5 +0,0 @@
-package com.alessandro.astages.integration.ftbquest;
-
-public interface AStageRewardType {
-//    RewardType A_STAGE = RewardTypes.register(new ResourceLocation("ftbquests", "astages"), AStageReward::new, () -> Icons.get("minecraft:item/emerald"));
-}
