457c3f9aa35dfd1a2730c797a0825b09c49053e6
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-06-17 17:10:18 +0100

    Clean up

--- src/main/java/com/alessandro/astages/util/AClientQuestionType.java
+++
@@ -1,8 +0,0 @@
-package com.alessandro.astages.util;
-
-@Deprecated(forRemoval = true)
-public enum AClientQuestionType {
-    TOOLTIP,
-    NAME,
-    JEI
-}
--- src/main/java/com/alessandro/astages/util/AClientRestriction.java
+++
@@ -1,4 +0,0 @@
-package com.alessandro.astages.util;
-
-@Deprecated(forRemoval = true)
-public interface AClientRestriction { }
