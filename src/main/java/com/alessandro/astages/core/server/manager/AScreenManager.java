e3641402973d5d9e3ce65988d1f4a657661df408
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-06-18 08:57:51 +0100

    Added custom checker for screen restrictions

--- src/main/java/com/alessandro/astages/core/server/manager/AScreenManager.java
+++ src/main/java/com/alessandro/astages/core/server/manager/AScreenManager.java
@@ -3,44 +3,66 @@ package com.alessandro.astages.core.server.manager;
 import com.alessandro.astages.core.server.restriction.AScreenRestriction;
 import com.alessandro.astages.store.server.AManager;
 import com.alessandro.astages.util.ARestrictionType;
-import com.alessandro.astages.util.OrderedMultiMap;
+import com.alessandro.astages.util.AStagesUtil;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class AScreenManager extends AManager<AScreenRestriction, MenuType<?>, MenuType<?>> {
-    public final OrderedMultiMap<MenuType<?>, AScreenRestriction> CACHE = OrderedMultiMap.create();
-
-    @Override
-    public void reloadBeforeScripts() {
-        super.reloadBeforeScripts();
-        CACHE.clear();
-    }
-
-    @Override
-    public void addRestriction(AScreenRestriction restriction) {
-        super.addRestriction(restriction);
-
-        for (var menu : restriction.getMenus()) {
-            CACHE.put(menu, restriction);
-        }
-    }
-
-    @Override
-    public AScreenRestriction getRestriction(Player player, MenuType<?> type) {
-        return getRestrictionFromCache(CACHE, type, player);
-    }
-
-    @Override
-    public void removeRestriction(String id) {
-        super.removeRestriction(id);
-        CACHE.removeValues(restriction -> restriction.getId().equals(id));
+public class AScreenManager extends AManager<AScreenRestriction, MenuType<?>, AbstractContainerMenu> {
+    public AScreenRestriction getRestriction(Player player, AbstractContainerMenu menu, @Nullable BlockState state, @Nullable BlockEntity entity) {
+        return getRestrictions().stream().filter(r -> !AStagesUtil.hasStage(player, r.getStage()) && r.isRestricted(menu, state, entity)).findFirst().orElse(null);
     }
 
     @Override
     public ARestrictionType associatedType() {
         return ARestrictionType.SCREEN;
     }
+
+//    public final OrderedMultiMap<MenuType<?>, AScreenRestriction> CACHE = OrderedMultiMap.create();
+
+//    @Override
+//    public void reloadBeforeScripts() {
+//        super.reloadBeforeScripts();
+//        CACHE.clear();
+//    }
+
+//    @Override
+//    public void addRestriction(AScreenRestriction restriction) {
+//        super.addRestriction(restriction);
+//
+//        for (var menu : restriction.getMenus()) {
+//            CACHE.put(menu, restriction);
+//        }
+//    }
+
+//    @Override
+//    public AScreenRestriction getRestriction(Player player, MenuType<?> type) {
+//        return getRestrictionFromCache(CACHE, type, player);
+//    }
+
+//    public AScreenRestriction getRestriction(Player player, MenuType<?> type, @Nullable BlockState state, @Nullable BlockEntity entity, @Nullable AbstractContainerMenu container) {
+//        var restrictions = CACHE.get(type);
+//
+//        if (!restrictions.isEmpty()) {
+//            for (var restriction : restrictions) {
+//                if (!AStagesUtil.hasStage(player, restriction.getStage()) && restriction.isRestricted(type, state, entity, container)) {
+//                    return restriction;
+//                }
+//            }
+//        }
+//
+//        return null;
+//    }
+
+//    @Override
+//    public void removeRestriction(String id) {
+//        super.removeRestriction(id);
+//        CACHE.removeValues(restriction -> restriction.getId().equals(id));
+//    }
 }
--- src/main/java/com/alessandro/astages/mixin/screen/AServerPlayer.java
+++ src/main/java/com/alessandro/astages/mixin/screen/AServerPlayer.java
@@ -1,11 +1,14 @@
 package com.alessandro.astages.mixin.screen;
 
 import com.alessandro.astages.core.ARestrictionManager;
+import com.alessandro.astages.core.server.restriction.AScreenRestriction;
 import com.alessandro.astages.store.Attributes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -18,10 +21,12 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import java.util.OptionalInt;
 
 @Mixin(value = ServerPlayer.class)
-public class AServerPlayer {
+public abstract class AServerPlayer {
     @Shadow public int containerCounter;
 
     @Shadow public ServerGamePacketListenerImpl connection;
+
+    @Shadow public abstract ServerLevel serverLevel();
 
     @Unique
     private ServerPlayer serverPlayer$self() {
@@ -29,8 +34,13 @@ public class AServerPlayer {
     }
 
     @Inject(method = "openMenu", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;send(Lnet/minecraft/network/protocol/Packet;)V"), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
-    public void astages$openMenu(MenuProvider pMenu, CallbackInfoReturnable<OptionalInt> cir, @NotNull AbstractContainerMenu abstractcontainermenu) {
-        var restriction = ARestrictionManager.SCREEN_INSTANCE.getRestriction(serverPlayer$self(), abstractcontainermenu.getType());
+    public void astages$openMenu(MenuProvider menu, CallbackInfoReturnable<OptionalInt> cir, @NotNull AbstractContainerMenu abstractcontainermenu) {
+        AScreenRestriction restriction;
+        if (menu instanceof BlockEntity entity) {
+            restriction = ARestrictionManager.SCREEN_INSTANCE.getRestriction(serverPlayer$self(), abstractcontainermenu, serverLevel().getBlockState(entity.getBlockPos()), entity);
+        } else {
+            restriction = ARestrictionManager.SCREEN_INSTANCE.getRestriction(serverPlayer$self(), abstractcontainermenu, null, null);
+        }
 
         if (restriction != null) {
             restriction.displayMessage(Attributes.Screen.OPEN_MESSAGE, abstractcontainermenu.getType(), serverPlayer$self());
