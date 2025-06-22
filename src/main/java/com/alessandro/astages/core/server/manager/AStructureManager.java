e2d84aec74b980dbef80e57f2b9cfaf486de95e3
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-06-22 14:37:27 +0200

    Added server stage support for structure restriction

--- src/main/java/com/alessandro/astages/core/server/manager/AStructureManager.java
+++ src/main/java/com/alessandro/astages/core/server/manager/AStructureManager.java
@@ -2,16 +2,19 @@ package com.alessandro.astages.core.server.manager;
 
 import com.alessandro.astages.core.server.restriction.AStructureRestriction;
 import com.alessandro.astages.event.structure.ServerEventHandler;
+import com.alessandro.astages.store.ServerStageReadable;
 import com.alessandro.astages.store.server.AManager;
 import com.alessandro.astages.util.ARestrictionType;
 import com.alessandro.astages.util.OrderedMultiMap;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
 @ParametersAreNonnullByDefault
-public class AStructureManager extends AManager<AStructureRestriction, ResourceLocation, ResourceLocation> {
+public class AStructureManager extends AManager<AStructureRestriction, ResourceLocation, ResourceLocation> implements ServerStageReadable<AStructureRestriction, ResourceLocation> {
     public final OrderedMultiMap<ResourceLocation, AStructureRestriction> CACHE = OrderedMultiMap.create();
 
     @Override
@@ -33,6 +36,26 @@ public class AStructureManager extends AManager<AStructureRestriction, ResourceL
     @Override
     public AStructureRestriction getRestriction(Player player, ResourceLocation structure) {
         return getRestrictionFromCache(CACHE, structure, player);
+    }
+
+    @Override
+    public AStructureRestriction getRestriction(MinecraftServer server, ResourceLocation structure) {
+        return getRestrictionFromCache(CACHE, structure, server);
+    }
+
+    @Override
+    public AStructureRestriction getRestriction(ResourceLocation structure, @Nullable Player player, @Nullable MinecraftServer server) {
+        AStructureRestriction serverRestriction = null;
+        AStructureRestriction playerRestriction = null;
+
+        if (server != null) { serverRestriction = getRestriction(server, structure); }
+        if (player != null) { playerRestriction = getRestriction(player, structure); }
+
+        if (serverRestriction == null) { // If the stage is unlocked in the server, pass!
+            return null;
+        }
+
+        return playerRestriction;
     }
 
     @Override
