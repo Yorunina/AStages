cb6e14294dbe24abef1edd95660ba459e8c26cf8
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-08-05 19:02:41 +0200

    Added default properties setters

--- src/main/java/com/alessandro/astages/store/Attributes.java
+++ src/main/java/com/alessandro/astages/store/Attributes.java
@@ -70,6 +70,11 @@ public class Attributes {
 
     public static final Attribute<ChatFormatting> CHAT_FORMATTING = Attribute.create("chat_formatting", AttributeTypes.CHAT_FORMATTING, ChatFormatting.GOLD);
 
+    @SuppressWarnings("unused")
+    public static void resetAllDefaultValues() {
+        AStages.ATTRIBUTES_REGISTRY.get().getValues().forEach(Attribute::resetDefaultValue);
+    }
+
     // If the creation of subclasses is HORRIBLE, change the default value to null and request, when you add an attribute to the attribute store, to check if a new default value is set
     // ITEM
     public static class Item {
