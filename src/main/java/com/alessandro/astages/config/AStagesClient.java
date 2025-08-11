7e2ad2223e5741d51848f0c4543eb8470cf1a644
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-08-11 19:06:44 +0200

    Tweaked config for experimental settings

--- src/main/java/com/alessandro/astages/config/AStagesClient.java
+++ src/main/java/com/alessandro/astages/config/AStagesClient.java
@@ -6,13 +6,21 @@ public class AStagesClient {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
     public static final ModConfigSpec SPEC;
 
-    public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_CLIENT_EXPERIMENTAL_SETTINGS;
+//    public static final ModConfigSpec.ConfigValue<Boolean> ENABLE_CLIENT_EXPERIMENTAL_SETTINGS;
+    public static final ModConfigSpec.ConfigValue<Boolean> LEVEL_CHUNK_EXPERIMENTAL_SETTINGS;
+    public static final ModConfigSpec.ConfigValue<Boolean> LEVEL_CHUNK_SECTION_EXPERIMENTAL_SETTINGS;
 
     static {
         BUILDER.push("Client Configs for AStages Mod");
 
-        ENABLE_CLIENT_EXPERIMENTAL_SETTINGS = BUILDER.comment("Enable experimental settings for ore restrictions, client side")
-                .define("Enable Experiments", false);
+//        ENABLE_CLIENT_EXPERIMENTAL_SETTINGS = BUILDER.comment("Enable experimental settings for ore restrictions, client side")
+//                .define("Enable Experiments", false);
+
+        LEVEL_CHUNK_EXPERIMENTAL_SETTINGS = BUILDER.comment("Enable experimental settings for LevelChunk class, client side")
+                .define("Enable Experiments for LevelChunk", false);
+
+        LEVEL_CHUNK_SECTION_EXPERIMENTAL_SETTINGS = BUILDER.comment("Enable experimental settings for LevelChunkSection class, client side")
+                .define("Enable Experiments for LevelChunkSection", false);
 
         BUILDER.pop();
 
