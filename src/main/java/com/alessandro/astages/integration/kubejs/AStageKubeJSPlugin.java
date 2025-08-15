c03df218ae0c184d579cb621e5be43ad52d2530e
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-08-15 17:53:49 +0200

    Exposed classes for config

--- src/main/java/com/alessandro/astages/integration/kubejs/AStageKubeJSPlugin.java
+++ src/main/java/com/alessandro/astages/integration/kubejs/AStageKubeJSPlugin.java
@@ -2,6 +2,9 @@ package com.alessandro.astages.integration.kubejs;
 
 import com.alessandro.astages.AStages;
 import com.alessandro.astages.core.ARestrictionManager;
+import com.alessandro.astages.core.server.restriction.*;
+import com.alessandro.astages.core.server.restriction.item.AItemRestriction;
+import com.alessandro.astages.core.server.restriction.recipe.ARecipeRestriction;
 import com.alessandro.astages.core.stage.AStageManager;
 import com.alessandro.astages.integration.Mods;
 import com.alessandro.astages.integration.kubejs.util.KubeJSStageEventHandler;
@@ -41,13 +44,36 @@ public class AStageKubeJSPlugin extends KubeJSPlugin {
         }
 
         event.add("AModels", AStagesModelJSUtil.class);
+        event.add("ATime", ATime.class);
+
         event.add("Attributes", Attributes.class);
         event.add("ItemAttributes", Attributes.Item.class);
-        event.add("ScreenAttributes", Attributes.Screen.class);
-        event.add("PetAttributes", Attributes.Pet.class);
+        // RecipeAttributes
+        // CropAttributes
         event.add("DimensionAttributes", Attributes.Dimension.class);
+        // EffectAttributes
+        // EnchantAttributes
+        // LootAttributes
+        event.add("MobAttributes", Attributes.Mob.class);
+        // OreAttributes
+        event.add("PetAttributes", Attributes.Pet.class);
+        event.add("RegionAttributes", Attributes.Region.class);
+        event.add("ScreenAttributes", Attributes.Screen.class);
         event.add("StructureAttributes", Attributes.Structure.class);
-        event.add("ATime", ATime.class);
+
+        event.add("AItemRestriction", AItemRestriction.class);
+        event.add("ARecipeRestriction", ARecipeRestriction.class);
+        event.add("ACropRestriction", ACropRestriction.class);
+        event.add("ADimensionRestriction", ADimensionRestriction.class);
+        event.add("AEffectRestriction", AEffectRestriction.class);
+        event.add("AEnchantRestriction", AEnchantRestriction.class);
+        event.add("ALootRestriction", ALootRestriction.class);
+        event.add("AMobRestriction", AMobRestriction.class);
+        event.add("AOreRestriction", AOreRestriction.class);
+        event.add("APetRestriction", APetRestriction.class);
+        event.add("ARegionRestriction", ARegionRestriction.class);
+        event.add("AScreenRestriction", AScreenRestriction.class);
+        event.add("AStructureRestriction", AStructureRestriction.class);
     }
 
     @Override
