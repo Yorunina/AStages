18bb3550fde58a83f133be99d4799407011f3697
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2025-11-01 10:54:51 +0100

    Minor changed

--- src/main/java/com/alessandro/astages/api/stage/event/ExpiredEvent.java
+++ src/main/java/com/alessandro/astages/api/stage/event/ExpiredEvent.java
@@ -3,7 +3,7 @@ package com.alessandro.astages.api.stage.event;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.entity.player.Player;
 
-public class ExpiredEvent extends GenericEvent{
+public class ExpiredEvent extends GenericEvent {
     public ExpiredEvent(Player player, MinecraftServer server, boolean isClientSide) {
         super(player, server, isClientSide);
     }
