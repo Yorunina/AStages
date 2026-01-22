aca1a109d806e7f7cfdf36a7d86cb9e2c3bf4dc3
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2026-01-22 21:19:46 +0100

    ci: Add GitHub release, temporary remove CurseForge and Modrinth integration

--- build.gradle.kts
+++ build.gradle.kts
@@ -317,35 +317,37 @@ publishMods {
     github {
         accessToken.set(providers.environmentVariable("GITHUB_TOKEN"))
         repository.set("Alessandro-Casale/AStages")
-        commitish.set("1.20.X")
-        tagName.set(mod_version)
+        val version = mod_version.substringBeforeLast("-")
+        val branch = mod_version.substringAfterLast("-")
+        commitish.set(branch)
+        tagName.set("v$version")
 
         announcementTitle.set("Download from GitHub")
     }
 
-    curseforge {
-        accessToken.set(providers.environmentVariable("CURSEFORGE_API_KEY"))
-        projectId.set("1120180")
-        minecraftVersions.add(minecraft_version)
-        changelogType.set("markdown")
-        optional("roughly-enough-items", "jei", "kubejs")
-
-        displayName.set("astages-$mod_version")
-
-        projectSlug.set("astages") // For discord setup
-        announcementTitle.set("Download from CurseForge") // For discord setup
-    }
-
-    modrinth {
-        accessToken.set(providers.environmentVariable("MODRINTH_API_KEY"))
-        projectId.set("6wy8fmIk")
-        minecraftVersions.add(minecraft_version)
-        optional("rei", "jei", "kubejs")
-
-        displayName.set("astages-$mod_version")
-
-        announcementTitle.set("Download from Modrinth")
-    }
+//    curseforge {
+//        accessToken.set(providers.environmentVariable("CURSEFORGE_API_KEY"))
+//        projectId.set("1120180")
+//        minecraftVersions.add(minecraft_version)
+//        changelogType.set("markdown")
+//        optional("roughly-enough-items", "jei", "kubejs")
+//
+//        displayName.set("astages-$mod_version")
+//
+//        projectSlug.set("astages") // For discord setup
+//        announcementTitle.set("Download from CurseForge") // For discord setup
+//    }
+//
+//    modrinth {
+//        accessToken.set(providers.environmentVariable("MODRINTH_API_KEY"))
+//        projectId.set("6wy8fmIk")
+//        minecraftVersions.add(minecraft_version)
+//        optional("rei", "jei", "kubejs")
+//
+//        displayName.set("astages-$mod_version")
+//
+//        announcementTitle.set("Download from Modrinth")
+//    }
 
 //    discord {
 //        webhookUrl.set(providers.environmentVariable("DISCORD_WEBHOOK"))
