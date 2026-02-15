5a6357faa24376c83f4012dab3178908a3784e56
Alessandro-Casale <alessandro.casale.developer@gmail.com>
2026-02-15 21:47:37 +0100

    api: Plugin integration for `Stage` and `TemporaryStage` classes

--- src/main/java/com/alessandro/astages/api/stage/Stage.java
+++ src/main/java/com/alessandro/astages/api/stage/Stage.java
@@ -1,6 +1,8 @@
 package com.alessandro.astages.api.stage;
 
 import com.alessandro.astages.api.nullability.NotNullParamsAndMethodsReturn;
+import com.alessandro.astages.core.AStageManager;
+import com.alessandro.astages.store.AttributeStore;
 
 @NotNullParamsAndMethodsReturn
 public class Stage extends BaseStage<Stage> {
@@ -10,5 +12,16 @@ public class Stage extends BaseStage<Stage> {
 
     public Stage(String stage, String description) {
         super(stage, description);
+    }
+
+    @Override
+    public AttributeStore allowedAttributes() {
+        var pluginAttributes = AStageManager.ATTACHED_ATTRIBUTES.getOrDefault(Stage.class, null);
+
+        if (pluginAttributes != null) {
+            return super.allowedAttributes().combineWith(pluginAttributes);
+        } else {
+            return super.allowedAttributes();
+        }
     }
 }
