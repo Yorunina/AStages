e79a328683473fac5c5343ed2742e9fd8b618409
alessandro <alessandro.casale.developer@gmail.com>
2026-07-12 21:02:04 +0200

    fix: Update AHolder and AStagesSuggestions to use ServerPlayer collection for command suggestions

--- src/main/java/com/alessandro/astages/api/command/AStagesSuggestions.java
+++ src/main/java/com/alessandro/astages/api/command/AStagesSuggestions.java
@@ -1,32 +1,33 @@
 package com.alessandro.astages.api.command;
 
-import com.alessandro.astages.api.holder.AClientHolder;
-import com.alessandro.astages.api.util.AStagesClientUtils;
-import com.alessandro.astages.engine.client.ClientMiscStorage;
+import com.alessandro.astages.api.holder.AHolder;
+import com.alessandro.astages.api.util.AStagesUtils;
+import com.alessandro.astages.engine.server.MiscStorage;
 import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.commands.arguments.EntityArgument;
 
 import java.util.HashSet;
 
 public class AStagesSuggestions {
     public static final SuggestionProvider<CommandSourceStack> PLAYER_ADD = (context, builder) -> {
-        var toReturn = new HashSet<>(ClientMiscStorage.ALL_STAGES);
-        AStagesClientUtils.getStages(AClientHolder.player()).forEach(toReturn::remove);
-        toReturn.removeAll(ClientMiscStorage.STAGES_ONLY_FOR_SERVER);
+        var toReturn = new HashSet<>(MiscStorage.ALL_STAGES);
+        AStagesUtils.getStages(AHolder.players(EntityArgument.getPlayers(context, "player"))).forEach(toReturn::remove);
+        toReturn.removeAll(MiscStorage.STAGES_ONLY_FOR_SERVER);
         return SharedSuggestionProvider.suggest(toReturn.stream().sorted(), builder);
     };
 
     public static final SuggestionProvider<CommandSourceStack> PLAYER_REMOVE = (context, builder) ->
-        SharedSuggestionProvider.suggest(AStagesClientUtils.getStages(AClientHolder.player()).stream().sorted(), builder);
+        SharedSuggestionProvider.suggest(AStagesUtils.getStages(AHolder.players(EntityArgument.getPlayers(context, "player"))).stream().sorted(), builder);
 
     public static final SuggestionProvider<CommandSourceStack> SERVER_ADD = (context, builder) -> {
-        var toReturn = new HashSet<>(ClientMiscStorage.ALL_STAGES);
-        AStagesClientUtils.getStages(AClientHolder.server()).forEach(toReturn::remove);
-        toReturn.removeAll(ClientMiscStorage.STAGES_ONLY_FOR_PLAYER);
+        var toReturn = new HashSet<>(MiscStorage.ALL_STAGES);
+        AStagesUtils.getStages(AHolder.server()).forEach(toReturn::remove);
+        toReturn.removeAll(MiscStorage.STAGES_ONLY_FOR_PLAYER);
         return SharedSuggestionProvider.suggest(toReturn.stream().sorted(), builder);
     };
 
     public static final SuggestionProvider<CommandSourceStack> SERVER_REMOVE = (context, builder) ->
-        SharedSuggestionProvider.suggest(AStagesClientUtils.getStages(AClientHolder.server()), builder);
+        SharedSuggestionProvider.suggest(AStagesUtils.getStages(AHolder.server()).stream().sorted(), builder);
 }
