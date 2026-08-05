578a247576a9136acd28e40050f0e3fac04b73f6
alessandro <alessandro.casale.developer@gmail.com>
2026-08-05 22:30:20 +0200

    api: Mark update events as not yet implemented and deprecate UnpackLootTableEvent

--- src/main/java/com/alessandro/astages/api/event/update/ClientItemUpdateEvent.java
+++ src/main/java/com/alessandro/astages/api/event/update/ClientItemUpdateEvent.java
@@ -1,5 +1,7 @@
 package com.alessandro.astages.api.event.update;
 
+import com.alessandro.astages.api.develop.NotYetImplemented;
 import net.minecraftforge.eventbus.api.Event;
 
+@NotYetImplemented("Must be refactored!")
 public class ClientItemUpdateEvent extends Event { }
